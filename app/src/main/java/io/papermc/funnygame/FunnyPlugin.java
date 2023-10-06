8a31217184c7f624c8d9ea66092260a8e65825c7
pairnon <janpairnon@gmail.com>
2023-10-05 21:32:03 -0700

    Limited time test

--- app/src/main/java/io/papermc/funnygame/FunnyPlugin.java
+++ app/src/main/java/io/papermc/funnygame/FunnyPlugin.java
@@ -7,6 +7,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.*;
+
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.boss.*;
+import org.bukkit.entity.*;
+
 public class FunnyPlugin extends JavaPlugin implements Listener {
 
     @Override
@@ -16,7 +22,28 @@ public class FunnyPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+
+        Player player = event.getPlayer();
+
+        player.sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+
+        FunnyPlayer funnyPlayer = new FunnyPlayer(player.getUniqueId(), 25);
+        
+        BossBar timeRem = Bukkit.createBossBar("x", BarColor.PURPLE, BarStyle.SOLID);
+        timeRem.addPlayer(player);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                funnyPlayer.decSecondsRemaining();
+                timeRem.setTitle("Seconds Remaining: " + funnyPlayer.getSecondsRemaining());
+                if(funnyPlayer.getSecondsRemaining() <= 0) {
+                    player.damage(1000000);
+                    funnyPlayer.setSecondsRemaining(25);
+                }
+            }
+        }.runTaskTimer(this, 0L, 20L);
+
     }
 
 }
