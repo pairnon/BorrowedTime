f4a1a14c888fa27c02940646fb444bcbf13d1804
pairnon <janpairnon@gmail.com>
2023-10-07 01:17:03 -0700

    Remove old comments

--- app/src/main/java/io/papermc/borrowedtime/commands/CommandSell.java
+++ app/src/main/java/io/papermc/borrowedtime/commands/CommandSell.java
@@ -13,7 +13,6 @@ import org.bukkit.entity.*;
 
 public class CommandSell implements CommandExecutor {
 
-    // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         
